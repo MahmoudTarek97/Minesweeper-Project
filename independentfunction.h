16a7da20a7d1459a92a18243432316b5091af2e3
Mahmoud Tarek <ma7moudedx@gmail.com>
2016-12-28 18:29:35 +0200

    Update independentfunction.h

--- independentfunction.h
+++ independentfunction.h
@@ -42,6 +42,7 @@ void welcomeScreen()
            "   *    ::::::    :   :     ::::: :::::  :       :::::   ::   ::    *\n"
            "   *                                                                *\n"
            "   ******************************************************************\n");
+    printf("   Made By Mahmoud Tarek & Mohamed El-shazly
     printf("\n                       PRESS <ENTER> TO CONTINUE..");
     char c;
     c=getchar();
